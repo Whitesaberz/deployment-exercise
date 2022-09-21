25e3639610e5a1d725cced7c28c946b87c5393a4
Whitesaber <woody.white276@gmail.com>
2022-09-21 14:42:23 -0600

    fixed GET

--- client/main.js
+++ client/main.js
@@ -1,1 +1,2 @@
 console.log("hello world");
+axios.post("/register");
--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/", express.static(path.join(__dirname, "../client/index.html")));
+
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.post("/register", (req, res) => res.status(200).send("Registered"));
+
 app.listen(port, () => {
   console.log("connected to port " + port);
 });
